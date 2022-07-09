0d4838eda469f081789fd2777f01435b753eca28
Valeriuna <106536158+Valeriuna@users.noreply.github.com>
2022-07-09 14:39:18 +0300

    add the footer

---
+++ src/java.js
@@ -0,0 +1,102 @@
+function formatDate(now) {
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayToday = now.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "Desember",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let day = days[dayToday];
+
+  return `${month} ${date}, ${year}, ${day} ${hours}:${minutes}`;
+}
+
+//Temperature
+
+function showTemperature(response) {
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  let cityName = document.querySelector("#search-city");
+  cityName.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#temperature");
+  currentTemperature.innerHTML = temperature;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity 💧  ${response.data.main.humidity} %`;
+  let wind = document.querySelector("#wind-speed");
+  wind.innerHTML = `Wind 💨 ${Math.round(response.data.wind.speed)} km/h`;
+}
+
+//City
+function search(city) {
+  let apiKey = "5c5db0dc3239cb6542e1902d9da964f5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  search(city);
+}
+
+//CurrentPosition
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "5c5db0dc3239cb6542e1902d9da964f5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentCity = document.querySelector("#current-location");
+currentCity.addEventListener("click", getCurrentCity);
+
+////
+
+let currentTime = document.querySelector("#current-time");
+let now = new Date();
+currentTime.innerHTML = formatDate(now);
+
+///
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+///
+search("Paris");
